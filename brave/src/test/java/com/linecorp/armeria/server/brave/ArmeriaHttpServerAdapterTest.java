c5be4aeb723c7d8737c9433d5763e51f2dde9f8b
koji.lin <koji.lin@gmail.com>
2019-07-26 13:59:15 +0900

    Remove space between path concat

--- brave/src/test/java/com/linecorp/armeria/server/brave/ArmeriaHttpServerAdapterTest.java
+++ brave/src/test/java/com/linecorp/armeria/server/brave/ArmeriaHttpServerAdapterTest.java
@@ -171,6 +171,6 @@ class ArmeriaHttpServerAdapterTest {
         final ServiceRequestContext context = mock(ServiceRequestContext.class);
         when(requestLog.context()).thenReturn(context);
         when(context.route()).thenReturn(Route.builder().pathWithPrefix("/foo/", "glob:bar").build());
-        assertThat(ArmeriaHttpServerAdapter.get().route(requestLog)).isEqualTo("/foo/ ^/(?:.+/)?bar$");
+        assertThat(ArmeriaHttpServerAdapter.get().route(requestLog)).isEqualTo("/foo/^/(?:.+/)?bar$");
     }
 }
